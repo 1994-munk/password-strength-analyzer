42fcf2b36fc0d95b79a72bacba1b2c29c0ed68e0
Shirleen Gatu <gatushirleen@gmail.com>
2026-05-31 17:27:43 +0300

    added entropy

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 from flask import Flask, render_template, request
 import re 
+import math 
+
+def calculate_entropy(password):
+    pool=0
+    if re.search(r'[a-z]', password):
+        pool += 26
+    if re.search(r'[A-Z]', password):
+        pool += 26
+    if re.search(r'\d', password):
+        pool += 10
+    if re.search(r'[!@#$%^&*()-+]', password):
+        pool += 32
+    if pool == 0:
+        return 0
+    return round(len(password) * math.log2(pool), 1)
+
+
+def entropy_label(bits):
+    if bits < 28:
+        return 'Very Weak'
+    elif bits < 36:
+        return 'Weak'
+    elif bits < 60:
+        return 'Moderate'
+    elif bits < 128:
+        return 'Strong'
+    else:
+        return 'Very Strong'
 #create a Flask application
 app = Flask(__name__)
 
@@ -50,8 +78,12 @@ def check_password_strength(password):
     else:
         strength = 'Strong'
         percentage = 100
+        
+    entropy = calculate_entropy(password)
+    e_label = entropy_label(entropy)
+    
 
-    return percentage, feedback, strength
+    return percentage, feedback, strength ,entropy, e_label
 
 #home route 
 @app.route("/", methods=['GET', 'POST'])
@@ -66,13 +98,15 @@ def home():
         
         password = request.form['password']
         
-        percentage, feedback, strength = check_password_strength(password)
+        percentage, feedback, strength ,entropy, e_label = check_password_strength(password)
         
     return render_template(
         'index.html',
         strength=strength,
         feedback=feedback,
-        percentage=percentage
+        percentage=percentage,
+        entropy=entropy,
+        e_label=e_label
     )
 
 #run the application
